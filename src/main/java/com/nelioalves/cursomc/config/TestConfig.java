ffaf20c41ef56665fb5214a8211b64f51257beba
Mardonio Silva da Costa <mrdonio.sc@gmail.com>
2020-03-16 12:03:28 -0300

    Criando o profile de teste

---
+++ src/main/java/com/nelioalves/cursomc/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.nelioalves.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nelioalves.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instatiateDatabase() throws Exception {
+		
+		dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+
+}
